a7a9a13b5fd4a016e4334bafe43ae5ae92135b53
Reee00 <ahmadd.renaldyy@gmail.com>
2024-09-08 22:21:07 +0700

    updated

--- assets/js/testimonial-new.js
+++ assets/js/testimonial-new.js
@@ -1,31 +1,38 @@
 const testimonials = [
     {
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRL5m2gr2lMb1huhMFwIR41jrDU5ZOxKydgEw&s",
-      content: "Dattebayo",
-      author: "Naruto",
+        "https://1.bp.blogspot.com/-_e1k3uZ0Klo/Xy_hTZl6NhI/AAAAAAAABbo/3a5WIKGo8KoCYTG4ux9N_Bj_WjyR7yvcgCNcBGAsYHQ/s400/DEE4342E-364D-4008-B5E8-619E9CF24CCD.jpeg",
+      content: "The Adventures",
+      author: "Jimmy Neutron",
       rating: 5,
     },
     {
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQFo9nKGI6eDtfB7wVLiJ0voKBJJb5nrJj9Wg&s",
-      content: "Tch",
-      author: "Sasuke",
+        "https://ceknricek.com/photo/plugin/article/2018/1528519003_1-org.jpg",
+      content: "Incredible",
+      author: "Baby",
       rating: 1,
     },
     {
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0MBAh1_8sj6rA6QhH31iBkhG2v7rLce4OSQ&s",
-      content: "Beban",
-      author: "Sakura",
+        "https://1.bp.blogspot.com/-1-NFUZrRXo4/Xy_ht3ZF3cI/AAAAAAAABb0/Vmw0zJZ4_l8R46wc5ildXb9JmlwuCNmcgCNcBGAsYHQ/s701/C0DF1B18-082B-48BF-BF18-618E75A83F49.jpeg",
+      content: "Dragon Ball",
+      author: "Goku",
+      rating: 2,
+    },
+    {
+      image:
+        "https://1.bp.blogspot.com/-TiqHg7-ima8/Xy_iteHmWCI/AAAAAAAABcc/vA_R0PfQY54q3aFKKf8pDiuadDl8T40ugCNcBGAsYHQ/s700/95F423B0-88C0-4FD4-9226-0FE196AA22BF.jpeg",
+      content: "OnePiece",
+      author: "Monkey D'Luffy",
       rating: 3,
     },
     {
       image:
-        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT0MBAh1_8sj6rA6QhH31iBkhG2v7rLce4OSQ&s",
-      content: "Beban",
-      author: "Sakura",
-      rating: 3,
+        "https://1.bp.blogspot.com/-Xcqo8OtOQgw/Xy_hfl2KX4I/AAAAAAAABbs/jbPkjUbV5bQFkpgESV5Hee3vqvLBDYUXQCNcBGAsYHQ/s640/95065F83-3704-4DD1-8292-59DEA209076C.jpeg",
+      content: "Captain",
+      author: "Tsubasa",
+      rating: 4,
     },
   ];
   
